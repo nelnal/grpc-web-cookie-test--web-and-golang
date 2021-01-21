11696095681827e8c549c49eca7aa2b733aa5672
Takashi Asaba <nelnal@gmail.com>
2021-01-21 14:22:41 +0900

    feat: add web front
    SSIA

---
+++ web/build.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const { build } = require("esbuild");
+
+const options = {
+  define: { "process.env.NODE_ENV": process.env.NODE_ENV | "" },
+  entryPoints: [path.resolve(__dirname, "src/main.ts")],
+  minify: false,
+  bundle: true,
+  target: "es2016",
+  platform: "browser",
+  outdir: path.resolve(__dirname, "html"),
+  tsconfig: path.resolve(__dirname, "tsconfig.json"),
+};
+
+build(options).catch((err) => {
+  process.stderr.write(err.stderr);
+  process.exit(1);
+});
